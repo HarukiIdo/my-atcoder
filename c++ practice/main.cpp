0b224e52c54da567149e6cab03e9fd128cff5010
Haruki Ido <rikuharu3705@outlook.jp>
2022-09-09 20:59:19 +0900

    update: READMEを更新しました

---
+++ c++ practice/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long lint;
+#define rep(i, n) for (lint i = 0; i < (lint)(n); i++)
+#define reps(i, s, n) for (lint i = (s); i < (lint)(n); i++)
+#define INF (1 << 30)
+
+int main() {
+  rep(i, 7){
+    cout << "こんちゃ" << endl;
+  }
+}
+
+
